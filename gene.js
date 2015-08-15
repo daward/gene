b71047005217bdc132e3e2bd296c54de88a139c5
daward <acheron0@gmail.com>
2015-08-14 23:39:40 -0400

    fixing packaging

---
+++ gene.js
@@ -0,0 +1,8 @@
+/*jslint node: true */
+/*jslint plusplus: true */
+'use strict';
+
+var God = require('./god.js')
+
+module.exports.Settings = require('./settings.json');
+module.exports.God = God;
